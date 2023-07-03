cb3ea5a7040b1a238c21b2962f1ce004e07007f2
RenanDevFelipe <128699439+RenanDevFelipe@users.noreply.github.com>
2023-07-03 07:50:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ const MenuBurger = document.querySelector(".menu-burger"),
         MenuPrincipal.classList.toggle("open-menu");
 
         MenuBurger.classList.toggle("lines");
-    })
+    });
+
+
+
+// DEIXAR FIXO COM SCROLL //
+
+window.addEventListener("scroll", function (){
+    var NavBar = document.querySelector(".nav-bar-principal");
+    var scrollTop = NavBar.scrollTop || document.documentElement.scrollTop; 
+    
+    if (scrollTop > 250){
+        NavBar.classList.add("nav-fixed-top");
+    }else{
+        NavBar.classList.remove("nav-fixed-top");
+    }
+});
